54d14819bd58557e97099283b3580f87442caf08
Nikhil Ranjan <nikhilranjan7@gmail.com>
2017-08-30 22:50:39 +0530

    Download data and read it with panda

---
+++ practice/linear_model.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+# Reding and labeling dataset
+train_file = tempfile.NamedTemporaryFile()
+test_file = tempfile.NamedTemporaryFile()
+train_file.name = "/data/train.csv"
+test_file.name = "/data/test.csv"
+
+CSV_COLUMNS = [
+    "age", "workclass", "fnlwgt", "education", "education_num",
+    "marital_status", "occupation", "relationship", "race", "gender",
+    "capital_gain", "capital_loss", "hours_per_week", "native_country",
+    "income_bracket"]
+
+df_train = pd.read_csv(train_file.name, names=CSV_COLUMNS, skipinitialspace=True)
+df_test = pd.read_csv(test_file.name, names=CSV_COLUMNS, skipinitialspace=True, skiprows=1)
