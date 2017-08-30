74fbb40a1d6dfb00dc533771e57c9dd28c5c2f86
Nikhil Ranjan <nikhilranjan7@gmail.com>
2017-08-30 13:48:24 +0530

    Complete basic operations

--- practice/operations.py
+++ practice/operations.py
@@ -21,3 +21,16 @@ mul = tf.multiply(a,b) # Supports Broadcasting
 with tf.Session() as sess:
     print("Addition with variables: %i" %sess.run(add, feed_dict={a:2, b:3}))
     print("Multiplication with variables: %i" % sess.run(mul, feed_dict={a: 2, b: 3}))
+
+#1x2 matrix
+matrix1 = tf.constant([[3., 3.]])
+#2x1 matrix
+matrix2 = tf.constant([[2.],[2.]])
+#tf.matmul is used to multiply matix, also supports broadcasting
+product = tf.matmul(matrix1, matrix2)
+
+#IMPORTANT: None of the above operations are executed untill session is run as it only create the graph
+
+with tf.Session() as sess:
+    result = sess.run(product)
+    print(result)
