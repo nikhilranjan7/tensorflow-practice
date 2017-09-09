65dfe1a48e2a8bc623d15aa9ef02f7733ea6e694
Nikhil Ranjan <nikhilranjan7@gmail.com>
2017-09-09 14:12:21 +0530

    Train model

--- machine-learning /linear_regression.py
+++ machine-learning /linear_regression.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import quandl
 import math
+import time
+import numpy as np
+from sklearn import preprocessing, model_selection, svm
+from sklearn.linear_model import LinearRegression
 
 df = quandl.get('WIKI/GOOGL')
 
@@ -22,6 +26,32 @@ forecast_out = int(math.ceil(0.01*len(df)))
 
 #This column is basically composed of Adj. Close of next 10 days i.e. first element is one 1 pair ahead of others if we have 100 datasets
 df['label'] = df[forecast_col].shift(-forecast_out)
-print(df.head())
 df.dropna(inplace=True)
-print(df.tail())
+print(df.head())
+# features include everything but label column
+x = np.array(df.drop(['label'],1))
+y = np.array(df['label'])
+
+#Goal is to get features values between -1 to 1 to increase accuracy
+x = preprocessing.scale(x)
+
+x_train, x_test, y_train, y_test = model_selection.train_test_split(x, y, test_size=0.2)
+
+
+# Simple linear Regression
+a = time.time()
+clf = LinearRegression()
+clf.fit(x_train, y_train)
+accuracy = clf.score(x_test, y_test)
+print(accuracy)
+b = time.time()
+print("LinearRegression took",round(b-a,3),"seconds")
+
+# SVM Regression
+a = time.time()
+clf = svm.SVR()
+clf.fit(x_train, y_train)
+accuracy = clf.score(x_test, y_test)
+print(accuracy)
+b = time.time()
+print("SVM took",round(b-a,3),"seconds")
