39df5b49c5fa4227ddf35144af7fa52a282bfab3
Nikhil Ranjan <nikhilranjan7@gmail.com>
2017-09-15 23:35:51 +0530

    Add gitignore

--- machine-learning/deep-net.py
+++ machine-learning/deep-net.py
@@ -1,5 +1,4 @@
 import tensorflow as tf
-
 '''
 Feed Forward network:
 input > weights > hidden layer 1 (activation function) > weights > hidden layer 2 (activation function) > weights > output layer
@@ -11,3 +10,7 @@ manipulate weight accordingly
 
 feed forward + backpropagation = epoch
 '''
+
+from tensorflow.examples.tutorials.mnist import input_data
+
+mnist = input_data.read_data_sets("data/", one_hot=True)
